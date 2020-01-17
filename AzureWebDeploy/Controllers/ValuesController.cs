843602c768a168c0d9d5ff95c1d0d60e94be794a
DiliniHB <dilinihb@gmail.com>
2020-01-17 08:36:10 +0000

    Update ValuesController.cs

--- AzureWebDeploy/Controllers/ValuesController.cs
+++ AzureWebDeploy/Controllers/ValuesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,29 +17,10 @@ namespace AzureWebDeploy.Controllers
             return new string[] { "value1", "value2new" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [HttpGet("GetUserId")]
+        public string GetUserId()
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return "Dilini Himali";
         }
     }
 }
